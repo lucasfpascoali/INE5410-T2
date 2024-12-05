6f4f7c1401e34af22c86db9cefcb90269508da6a
luccafpascoali <luccafpascoali@gmail.com>
2024-12-05 14:53:59 -0300

    Implementação de acordo com o enunciado. Deadlock na saída da atração

--- nasa.py
+++ nasa.py
@@ -2,7 +2,7 @@ import sys
 import random
 import queue
 from time import sleep
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 from threading import Thread, current_thread, Lock, Semaphore
 
 
@@ -45,53 +45,102 @@ def criar_atracoes(n_atracoes: int) -> List[str]:
 
 def criar_pessoas(n_pessoas: int) -> List[Thread]:
     global atracoes
-    return [Thread(target=rotina_pessoa, args=(atracoes[random.randint(0,n_atracoes-1)],)) for i in range(n_pessoas)]
+    pessoas = []
+    for i in range(n_pessoas):
+        pessoas.append(Thread(
+            target=rotina_pessoa, 
+            args=(
+                atracoes[random.randint(0,n_atracoes-1)],
+                Semaphore(0)
+            )
+        ))
 
-def rotina_pessoa(atracao: str):
-    global fila, ordem, lock_ordem, atracao_atual, quantidade_pessoas_na_atracao, lock_quantidade_pessoas_na_atracao, sem_pessoas_na_atracao, sem_iniciar_atracao
-    # pessoa = current_thread()
+    return pessoas
+
+def rotina_gerador_pessoas() -> None:
+    global max_intervalo, n_pessoas
+    pessoas = criar_pessoas(n_pessoas)
+    for p in pessoas:
+        sleep(random.randint(0, max_intervalo))
+        p.start()
+    
+    for p in pessoas: p.join()
+    
+
+def rotina_pessoa(atracao: str, sem: Semaphore) -> None:
+    global fila_entrar, fila_sair, lock_ordem, lock_qtd_pessoas_atracao, ordem, qtd_pessoas_atracao, sem_pessoas_na_fila_entrar, sem_proxima_atracao, sem_total_vagas, tempo_permanecia, unidade_tempo
 
     with lock_ordem:
-        # fila.put(pessoa)
         priv_ordem = ordem
-        print(f'[Pessoa {priv_ordem} / {atracao}] Aguardando na fila.')
         ordem += 1
+        fila_entrar.put({'atracao': atracao, 'ordem': priv_ordem, 'semaforo': sem}) # Coloca pessoa na fila
+        print(f"[Pessoa {priv_ordem} / {atracao}] Aguardando na fila_entrar.")
+        sem_pessoas_na_fila_entrar.release() # Sinaliza que tem pessoa na fila
+
+    # Pessoa aguarda a vez dela na fila_entrar
+    sem.acquire()
+
+    # Pessoa entra na atração
+    with lock_qtd_pessoas_atracao:
+        qtd_pessoas_atracao += 1
+        print(f"[Pessoa {priv_ordem} / {atracao}] Entrou na NASA Experiences (quantidade = {qtd_pessoas_atracao}).")
     
-    if atracao_atual != atracao:
-        sem_iniciar_atracao.acquire()
-        atracao_atual = atracao
-        print(f'[NASA] Iniciando a experiência {atracao_atual}.')
-    
-    sem_pessoas_na_atracao.acquire()
-    with lock_quantidade_pessoas_na_atracao:
-        quantidade_pessoas_na_atracao += 1
-        print(f'[Pessoa {priv_ordem} / {atracao}] Entrou na NASA Experiences (quantidade = {quantidade_pessoas_na_atracao}).')
+  
+    fila_sair.put(fila_entrar.get()) # Pessoa sai da fila de entrada e entra na fila de saída
+    sem_proxima_pessoa.release() # Libera proxima pessoa da fila_entrar
 
     sleep(tempo_permanecia * unidade_tempo)
 
-    with lock_quantidade_pessoas_na_atracao:
-        quantidade_pessoas_na_atracao -= 1
-        print(f'[Pessoa {priv_ordem} / {atracao}] Saiu da NASA Experiences (quantidade = {quantidade_pessoas_na_atracao}).')
-        sem_pessoas_na_atracao.release()
-        if quantidade_pessoas_na_atracao == 0:
-            sem_iniciar_atracao.release()
+    with fila_sair.mutex:
+        primeiro_a_sair_atracao = fila_sair.queue[0]
+             
+    if primeiro_a_sair_atracao["ordem"] != priv_ordem:
+        sem.acquire() # Pessoa aguarda para sair
+
+    with lock_qtd_pessoas_atracao:
+        qtd_pessoas_atracao -= 1
+        print(f"[Pessoa {priv_ordem} / {atracao}] Saiu da NASA Experiences (quantidade = {qtd_pessoas_atracao}).")
+        if qtd_pessoas_atracao == 0: sem_proxima_atracao.release()
+        with fila_sair.mutex:
+            fila_sair.get() # Pessoa sai da atração
+            if not fila_sair.empty():
+                proximo_a_sair_atracao = fila_sair.queue[0]
+                proximo_a_sair_atracao["semaforo"].release() # Libera o proximo da fila de saída para sair
+            sem_total_vagas.release()
+        
+
 
             
             
 
 def rotina_nasa():
-    global n_pessoas, max_intervalo
+    global atracao_atual, fila_entrar, n_pessoas, sem_pessoas_na_fila_entrar, sem_proxima_pessoa, sem_total_vagas
 
-    pessoas = criar_pessoas(n_pessoas)
-    print('[NASA] Simulacao iniciada.')
-    for p in pessoas:
-        sleep(random.randint(0, max_intervalo))
-        p.start()
+    print("[NASA] Simulacao iniciada.")
 
-    for p in pessoas:
-        p.join()
-    
-    print('[NASA] Simulacao finalizada.')
+    for i in range(0, n_pessoas):
+        if atracao_atual != '' and qtd_pessoas_atracao == 0 and fila_entrar.empty():
+            print(f"[NASA] Pausando a experiencia {atracao_atual}.")
+
+        sem_pessoas_na_fila_entrar.acquire() # Aguarda ter pessoas na fila
+
+        with fila_entrar.mutex:
+            pessoa = fila_entrar.queue[0]
+        
+        # Atração da primeia pessoa da fila é diferente da atração atual
+        if pessoa['atracao'] != atracao_atual:
+            sem_proxima_atracao.acquire() # Espera acabar atração
+            atracao_atual = pessoa['atracao'] # Atualiza atração atual
+            print(f"[NASA] Iniciando a experiencia {atracao_atual}.")
+
+        sem_total_vagas.acquire() # Espera ter vagas suficientes
+        pessoa['semaforo'].release() # Libera thread da Pessoa
+        
+        sem_proxima_pessoa.acquire() # Aguarda pessoa entrar na atração
+
+    print("[NASA] Simulacao finalizada.")
+        
+            
     
 
         
@@ -104,16 +153,30 @@ if __name__ == '__main__':
     # Configurando a semente
     random.seed(semente)
 
-    atracoes = criar_atracoes(n_atracoes)
-    atracao_atual = ''
+    # Variáveis Globais
+    fila_entrar = queue.Queue(n_pessoas)
+    fila_sair = queue.Queue(n_pessoas)
     ordem = 1
+    atracao_atual = ''
+    qtd_pessoas_atracao = 0
+    
+    # Semáforos e Mutexes
     lock_ordem = Lock()
-    lock_quantidade_pessoas_na_atracao = Lock()
-    sem_pessoas_na_atracao = Semaphore(n_vagas)
-    sem_iniciar_atracao = Semaphore(1)
-    quantidade_pessoas_na_atracao = 0
-    fila = queue.Queue()
+    lock_qtd_pessoas_atracao = Lock()
+    sem_pessoas_na_fila_entrar = Semaphore(0)
+    sem_proxima_pessoa = Semaphore(0)
+    sem_total_vagas = Semaphore(n_vagas)
+    sem_proxima_atracao = Semaphore(1)
+
+    atracoes = criar_atracoes(n_atracoes)
+
     nasa = Thread(target=rotina_nasa)
+    gerador_pessoas = Thread(target=rotina_gerador_pessoas)
 
     nasa.start()
+    gerador_pessoas.start()
+
+    gerador_pessoas.join()
     nasa.join()
+
+    
